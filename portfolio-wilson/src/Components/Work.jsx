cbe1d673b06e6d8f4a7a4c70a44c4f42001d77a8
wilsonjoseph-dev <wilsoneee21@gmail,com>
2025-02-18 12:42:15 +0530

    alter the work section

--- portfolio-wilson/src/Components/Work.jsx
+++ portfolio-wilson/src/Components/Work.jsx
@@ -11,7 +11,7 @@ const data = [
 },
   {
     year:2023,
-    title:'digital marketting intern',
+    title:'digital marketing intern',
     duration:'2 month',
     details:
     'As a Digital Marketing Specialist at XYZ Company, I developed and executed strategies to drive online traffic and generate leads. Through optimized Google AdWords campaigns, I achieved a 30% increase in click-through rates and a 20% reduction in CPA. Additionally, my SEO efforts led to a 40% surge in website traffic within six months. Crafting engaging social media content and targeted email campaigns resulted in a 50% growth in followers and improved email open rates by 25% and click-through rates by 15%.'
